04979762c711ddd1794eb8f8d94c8010f2ce8a0c
JokerJer1 <jermaine.vanheyningen@gmail.com>
2024-11-16 18:16:02 -0500

    added fuzz search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from thefuzz import fuzz, process
 
 '''
 with open('games.csv', mode = 'r') as file:
@@ -7,9 +8,21 @@ with open('games.csv', mode = 'r') as file:
         print(line[0])
 '''
 
+def fuzz_match(str1, str2):
+    fuzz1 = fuzz.ratio(str1, str2)
+    fuzz2 = fuzz.partial_ratio(str1, str2)
+    w1 = .7
+    w2 = .3
+
+    return (fuzz1 * w2) + (fuzz2 * w1)
+
+
+
 with open('games.csv', mode = 'r') as file:
-    game_title = input("Enter a game: ")
+    game_title = input("Enter a game: ").lower()
     csvFile = csv.reader(file)
     for line in csvFile:
-        if game_title in line:
-            print(f"The game {line[0]} is worth ${line[1]}!")
+        if fuzz_match(game_title, line[0]) >= 70:
+            print(f"{line[0]} for {line[3]} is worth ${line[1]}!")
+            print(fuzz_match(game_title, line[0]))
+            
