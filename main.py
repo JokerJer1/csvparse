3f3a5328a24b43616f3f90d226120488381c8ecb
JokerJer1 <jermaine.vanheyningen@gmail.com>
2024-11-16 23:45:58 -0500

    added prompt after & recursion

--- main.py
+++ main.py
@@ -11,18 +11,25 @@ with open('games.csv', mode = 'r') as file:
 def fuzz_match(str1, str2):
     fuzz1 = fuzz.ratio(str1, str2)
     fuzz2 = fuzz.partial_ratio(str1, str2)
-    w1 = .7
-    w2 = .3
+    w1 = .9
+    w2 = .1
 
     return (fuzz1 * w2) + (fuzz2 * w1)
 
 
+def game():
+    with open('games.csv', mode = 'r') as file:
+        game_title = input("Enter a game: ").lower()
+        csvFile = csv.reader(file)
+        for line in csvFile:
+            if fuzz_match(game_title, line[0]) >= 70:
+                print(f"\n{line[0]} for {line[3]} is worth ${line[1]}!\n")
+        try2 = input("Would you like to try another title? y/n : ")
+        if try2 == 'y':
+            game()
+        else:
+            print("Thanks for using Game Parser!")
+            return
+        
+game()
 
-with open('games.csv', mode = 'r') as file:
-    game_title = input("Enter a game: ").lower()
-    csvFile = csv.reader(file)
-    for line in csvFile:
-        if fuzz_match(game_title, line[0]) >= 70:
-            print(f"{line[0]} for {line[3]} is worth ${line[1]}!")
-            print(fuzz_match(game_title, line[0]))
-            
