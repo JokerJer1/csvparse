907333530670bad55f6056f0a2d54bea0c2e6cc5
JokerJer1 <jermaine.vanheyningen@gmail.com>
2024-11-17 18:32:48 -0500

    added txt file and display

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ def fuzz_match(str1, str2):
 
     return (fuzz1 * w2) + (fuzz2 * w1)
 
+def save_games(game):
+    f = open("games.txt", 'a')
+    f.write(game + '\n')
+
+def display_totals():
+    f = open("games.txt", 'r')
+    print('\n')
+    print(f.read())
+    print('\n')
+
+def clear_file():
+    f = open("games.txt", 'r+')
+    f.truncate(0)
+
+
 
 def game():
     with open('games.csv', mode = 'r') as file:
@@ -24,11 +39,14 @@ def game():
         for line in csvFile:
             if fuzz_match(game_title, line[0]) >= 70:
                 print(f"\n{line[0]} for {line[3]} is worth ${line[1]}!\n")
+                save_games(line[0] + " " + line[1])
         try2 = input("Would you like to try another title? y/n : ")
         if try2 == 'y':
             game()
         else:
+            display_totals()
             print("Thanks for using Game Parser!")
+            clear_file()
             return
         
 game()
